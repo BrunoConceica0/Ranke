0d6abb033c815ee226d88cc6ceecba7e4a5ac8ec
BrunoConceica0 <bd687353@gmail.com>
2024-10-22 22:08:12 -0300

    Estilizando os inputs e produtos

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,14 +1,21 @@
 <template>
-  <section class="produto-container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <img
-        v-if="produto.fotos"
-        :src="produto.fotos[0].src"
-        :alt="produto.fotos[0].titulo"
-      />
-      <h2 class="titulo">{{ produto.nome }}</h2>
-      <p class="preco">{{ produto.preco }}</p>
-      <p class="descricao">{{ produto.descricao }}</p>
+  <section class="produtos-container">
+    <div v-if="produtos && produtos.length > 0" class="produtos">
+      <div v-for="produto in produtos" :key="produto.id" class="produto">
+        <router-link to="/">
+          <img
+            v-if="produto.fotos"
+            :src="produto.fotos[0].src"
+            :alt="produto.fotos[0].titulo"
+          />
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p class="preco">{{ produto.preco }}</p>
+          <p class="descricao">{{ produto.descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="produtos && produtos.length === 0" class="sem-resultados">
+      <p>Busca sem resultado. Tente buscar outro termo.</p>
     </div>
   </section>
 </template>
@@ -66,4 +73,44 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 30px;
+  margin: 30px;
+}
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+.produto:hover {
+  transform: scale(1.1);
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  position: relative;
+  z-index: 1;
+}
+.produto img {
+  width: 100%;
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+.titulo {
+  margin-bottom: 10px;
+}
+.preco {
+  font-weight: bold;
+  color: #e80;
+}
+.sem-resultados {
+  text-align: center;
+  
+}
+</style>
